741f2a10a82ae3519624064f76520134c6be8da4
unknown <mdsakibhr7@gmail.com>
2023-09-28 00:03:59 +0600

    add a new form for registion

--- registration.php
+++ registration.php
@@ -1,68 +1,66 @@
 <?php include 'header.php' ?>
-<form action="" method="post">
-    <section class="vh-100" style="background-color: #eee;">
 
-        <div class="container h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-lg-12 col-xl-11">
-                    <div class="card text-black" style="border-radius: 25px;">
-                        <div class="card-body p-md-5">
-                            <div class="row justify-content-center">
-                                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Registration</p>
+<section class="vh-100" style="background-color: #eee;">
+    <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-lg-12 col-xl-11">
+                <div class="card text-black" style="border-radius: 25px;">
+                    <div class="card-body p-md-5">
+                        <div class="row justify-content-center">
+                            <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                                    <form class="mx-1 mx-md-4">
+                                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Registration</p>
 
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                                            <div class="form-outline flex-fill mb-0">
-                                                <input type="text" id="form3Example1c" class="form-control"/>
-                                                <label class="form-label" for="form3Example1c">First Name</label>
-                                            </div>
-                                        </div>
+                                <form action="" method="POST">
+                                    <div class="mb-4">
+                                        <label class="form-label" for="firstName">First Name</label>
+                                        <input type="text" id="firstName" name="firstName" class="form-control">
+                                    </div>
 
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                                            <div class="form-outline flex-fill mb-0">
-                                                <input type="email" id="form3Example3c" class="form-control"/>
-                                                <label class="form-label" for="form3Example3c">Email</label>
-                                            </div>
-                                        </div>
+                                    <div class="mb-4">
+                                        <label class="form-label" for="email">Email</label>
+                                        <input type="email" id="email" name="email" class="form-control">
+                                    </div>
 
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                                            <div class="form-outline flex-fill mb-0">
-                                                <input type="text" id="form3Example4cd" class="form-control"/>
-                                                <label class="form-label" for="form3Example4cd">Mobile</label>
-                                            </div>
-                                        </div>
+                                    <div class="mb-4">
+                                        <label class="form-label" for="mobile">Mobile</label>
+                                        <input type="text" id="mobile" name="mobile" class="form-control">
+                                    </div>
 
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                                            <div class="form-outline flex-fill mb-0">
-                                                <input type="password" id="form3Example4c" class="form-control"/>
-                                                <label class="form-label" for="form3Example4c">Password</label>
-                                            </div>
-                                        </div>
+                                    <div class="mb-4">
+                                        <label class="form-label" for="role">Role</label>
+                                        <select class="form-select" id="role" name="role" aria-label="Select Role">
+                                            <option selected disabled>Select Role</option>
+                                            <option value="1">Admin</option>
+                                            <option value="2">Customer</option>
+                                        </select>
+                                    </div>
 
-                                        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                            <button type="button" class="btn btn-primary btn-lg">Register</button>
-                                        </div>
+                                    <div class="mb-4">
+                                        <label class="form-label" for="password">Password</label>
+                                        <input type="password" id="password" name="password" class="form-control">
+                                    </div>
 
-                                    </form>
+                                    <div class="text-center mb-3 mb-lg-4">
+                                        <button type="submit" class="btn btn-primary btn-lg">Register</button>
+                                    </div>
 
-                                </div>
-                                <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                                </form>
 
-                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
-                                         class="img-fluid" alt="Sample image">
-                                </div>
+                            </div>
+                            <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                                     class="img-fluid" alt="Sample image">
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-</form>
+    </div>
+</section>
+
+<?php
+include 'footer.php'; ?>
